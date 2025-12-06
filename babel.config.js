b0063e69fc1c90d1e0227197d1cf314a15f97803
Koi-12-love <khoihuynh152004@gmail.com>
2025-12-06 12:58:57 +0700

    WIP

--- babel.config.js
+++ babel.config.js
@@ -5,5 +5,9 @@ module.exports = function (api) {
             ["babel-preset-expo", { jsxImportSource: "nativewind" }],
             "nativewind/babel",
         ],
+        plugins: [
+            // Required for Reanimated 4.x
+            "react-native-reanimated/plugin",
+        ],
     };
 };
--- metro.config.js
+++ metro.config.js
@@ -1,13 +1,23 @@
-const { withNativeWind } = require("nativewind/metro");
-const { getDefaultConfig } = require("expo/metro-config");
-const { getSentryExpoConfig } = require("@sentry/react-native/metro");
+// metro.config.js – HOÀN HẢO cho Expo SDK 54 + NativeWind v4 + Sentry
+const { getDefaultConfig } = require('expo/metro-config');
+const { withNativeWind } = require('nativewind/metro');
 
-let config = getDefaultConfig(__dirname);
+// Lấy config mặc định của Expo
+const defaultConfig = getDefaultConfig(__dirname);
 
-config = getSentryExpoConfig(__dirname, config);
+// Áp dụng Sentry trước (nếu có)
+let config = defaultConfig;
+try {
+  const { getSentryExpoConfig } = require('@sentry/react-native/metro');
+  config = getSentryExpoConfig(__dirname, config);
+} catch (e) {
+  // Nếu không có Sentry thì bỏ qua
+}
 
-config = withNativeWind(config, {
-  input: "./app/globals.css",
+// QUAN TRỌNG: Dùng cách wrap này để tránh lỗi resolver + transformer
+const finalConfig = withNativeWind(config, {
+  input: './app/globals.css',
+  configPath: './tailwind.config.js', // nếu em có file này
 });
 
-module.exports = config;
+module.exports = finalConfig;
